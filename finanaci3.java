0a9470f049ebba76b0ce4d99e501999d8ab18fe8
tarunpodila <92811084+tarunpodila@users.noreply.github.com>
2021-11-26 15:44:46 +0530

    Create finanaci3.java

---
+++ finanaci3.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+  
+public class Main
+{
+    
+    public static int gcdExtended(int a, int b, int x, int y)
+    {
+        
+        if (a == 0)
+        {
+            x = 0;
+            y = 1;
+            return b;
+        }
+  
+        int x1=1, y1=1; 
+        int gcd = gcdExtended(b%a, a, x1, y1);
+  
+        
+        x = y1 - (b/a) * x1;
+        y = x1;
+  
+        return gcd;
+    }
+  
+
+    public static void main(String[] args)
+    {
+        int x=1, y=1;
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int g = gcdExtended(a, b, x, y);
+        System.out.print("gcd(" + a +  " , " + b+ ") = " + g);
+  
+    }
+}
